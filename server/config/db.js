3e8a9000acaf40ae1f499525c822a1b4150f90f7
Michael-Angelo Dinuccio <mdinucci@my.centennialcollege.ca>
2021-10-20 15:32:48 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
+    // "URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://maddinuc98:VWJnnsQqManZCpuo@mongodbserver.b60xq.mongodb.net/book_store?retryWrites=true&w=majority"
 }
